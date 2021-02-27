e8cecaddb889db9d32303a2c4a085d204fd44d2a
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2021-02-27 16:22:29 -0300

    Create a sample level, PlayerController and CameraController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 5f;
+    public float JumpHeight = 2f;
+    public float GroundDistance = 0.2f;
+    public float DashDistance = 5f;
+    public LayerMask Ground;
+
+    private Rigidbody _body;
+    private Vector3 _inputs = Vector3.zero;
+    private bool _isGrounded = true;
+    private Transform _groundChecker;
+
+    private void Start()
+    {
+        _body = GetComponent<Rigidbody>();
+        _groundChecker = gameObject.transform.GetChild(0);
+        //_groundChecker = gameObject.transform;
+    }
+
+    private void Update()
+    {
+        _isGrounded = Physics.CheckSphere(_groundChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
+        //Debug.Log(_isGrounded);
+
+        _inputs = Vector3.zero;
+        _inputs.x = Input.GetAxis("Horizontal");
+        //_inputs.z = Input.GetAxis("Vertical");
+        if (_inputs != Vector3.zero)
+            transform.forward = _inputs;
+            
+        Jump();
+        Dash();
+
+        MovePlayerToStart();
+    }
+
+    private void FixedUpdate()
+    {
+        _body.MovePosition(_body.position + _inputs * Speed * Time.fixedDeltaTime);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            Debug.Log("Jump");
+            _body.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+    }
+
+    private void Dash()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Debug.Log("Dash");
+            Vector3 dashVelocity = Vector3.Scale(transform.forward, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime)));
+            _body.AddForce(dashVelocity, ForceMode.VelocityChange);
+        }
+    }
+
+    private void MovePlayerToStart()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Vector3 startPosition = new Vector3(0, 1f, 0);
+            _body.MovePosition(startPosition);
+        }
+    }
+}
