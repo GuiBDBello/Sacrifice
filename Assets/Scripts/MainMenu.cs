c0e8cb9042579e315f54153f1d2ba4aba8fe6fb7
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2021-02-28 17:30:43 -0300

    Create main menu and Add Effects from Unity Particle Pack

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(int level)
+    {
+        SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + level);
+    }
+
+    public void Settings()
+    {
+
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("Goodbye World!");
+        Application.Quit();
+    }
+}
