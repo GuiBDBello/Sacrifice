3d31b598c7a6b61c0139f349c44dc747d16253e1
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2021-02-28 19:02:58 -0300

    Add grenade as a prefab; Update player mesh

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,23 +9,22 @@ public class PlayerController : MonoBehaviour
     public float GroundDistance = 0.2f;
     public float DashDistance = 5f;
     public LayerMask Ground;
+    public Transform GroundChecker;
+    public GameObject Grenade;
+    public float ThrowForce = 10f;
 
     private Rigidbody _body;
     private Vector3 _inputs = Vector3.zero;
     private bool _isGrounded = true;
-    private Transform _groundChecker;
 
     private void Start()
     {
         _body = GetComponent<Rigidbody>();
-        _groundChecker = gameObject.transform.GetChild(0);
-        //_groundChecker = gameObject.transform;
     }
 
     private void Update()
     {
-        _isGrounded = Physics.CheckSphere(_groundChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
-        //Debug.Log(_isGrounded);
+        _isGrounded = Physics.CheckSphere(GroundChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
 
         _inputs = Vector3.zero;
         _inputs.x = Input.GetAxis("Horizontal");
@@ -33,9 +32,15 @@ public class PlayerController : MonoBehaviour
         if (_inputs != Vector3.zero)
             transform.forward = _inputs;
             
-        Jump();
-        Dash();
-
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+            Jump();
+        
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            Dash();
+        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            ThrowGrenade();
+        
         MovePlayerToStart();
     }
 
@@ -46,21 +51,15 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
-        {
-            Debug.Log("Jump");
-            _body.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
-        }
+        Debug.Log("Jump");
+        _body.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
     }
 
     private void Dash()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            Debug.Log("Dash");
-            Vector3 dashVelocity = Vector3.Scale(transform.forward, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime)));
-            _body.AddForce(dashVelocity, ForceMode.VelocityChange);
-        }
+        Debug.Log("Dash");
+        Vector3 dashVelocity = Vector3.Scale(transform.forward, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime)));
+        _body.AddForce(dashVelocity, ForceMode.VelocityChange);
     }
 
     private void MovePlayerToStart()
@@ -70,5 +69,12 @@ public class PlayerController : MonoBehaviour
             Vector3 startPosition = new Vector3(0, 1f, 0);
             _body.MovePosition(startPosition);
         }
+    }
+
+    private void ThrowGrenade()
+    {
+        GameObject grenade = Instantiate(Grenade, transform.position + (transform.forward), transform.rotation);
+        Rigidbody rb = grenade.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * ThrowForce, ForceMode.VelocityChange);
     }
 }
